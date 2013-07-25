ce6ab92d50a099ea5491987f9fb3601c8d75ac5c
Jáchym Toušek <enumag@gmail.com>
2013-07-25 10:31:04 +0200

    Removed guys from repository

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -3,4 +3,5 @@
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml src
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml tests/unit
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml tests/integration
+call "vendor/bin/codecept.bat" build
 call "vendor/bin/codecept.bat" run --silent
