2911c5490917faa7b357a602db3f8a8eb3bce778
Jáchym Toušek <enumag@gmail.com>
2017-09-30 11:51:26 +0200

    Use strict types

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Arachne\SecurityVerification;
 
 use Arachne\SecurityVerification\Exception\InvalidArgumentException;
--- src/Rules/IdentityRuleHandler.php
+++ src/Rules/IdentityRuleHandler.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Arachne\SecurityVerification\Rules;
 
 use Arachne\SecurityVerification\Exception\InvalidArgumentException;
